72300799a5497cca034585e349ef21185494fdc0
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-09 12:07:38 -0500

    commit tree ba33198fa5ec65a627db9002bf20ddd02fd5c354

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  newArray = []
+  names.each {|name| newArray <<  badge_maker(name)}
+  newArray
+end
+
+def assign_rooms(speakers)
+    array = []
+    speakers.each_with_index do |name, index|
+    array << "Hello, #{name}! You'\ll be assigned to room #{index+1}!"
+    end
+    array  
+def printer(attendees)
+      batch_badge_creator(attendees).each { |names| puts names }
+      assign_rooms(attendees).each { |room| puts room }
+      end
+end
